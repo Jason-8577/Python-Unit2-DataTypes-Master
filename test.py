4350237cd151f53507a6a7aa5d67e4d1fe505b1a
Jason-8577 <jasono119@nycstudents.net>
2025-09-17 06:16:10 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-x = "welcome, come in"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
+""" homework = True
+
+def not_movies(homework):
+    if not homework:
+        print("movie time")
+    else:
+        print("homework time, I hate russian")
+not_movies(homework) """
+
+yes = True 
