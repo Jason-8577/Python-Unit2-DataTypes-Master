8d4701d3d5e65e90b006727548865693c71cf39d
Jason-8577 <jasono119@nycstudents.net>
2025-09-22 06:05:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,20 +7,22 @@ def not_movies(homework):
         print("homework time, I hate russian")
 not_movies(homework) """
 
-""" x = 2
-if x == x:
-    print("even")
-else:
-    print("odd") """
-""" 
-def factor(x,y):
+""" def factor(x,y):
     if x % y == 0:
         print(f"{y} is a factor of {x}")
-factor(20,5) """
-""" 
+factor(20,5) 
+
 a = int('5')
 bill = input("How much was the bill?")
-print(int(bill))
+print(int(bill))  """
 
- """
-
+print("Was the service good")
+def tip(y):
+    if y == 0:
+        print("bad")
+    elif y == 15:
+        print("okay")
+    if y == 20:
+        print("good")
+    else:
+        print("great")
