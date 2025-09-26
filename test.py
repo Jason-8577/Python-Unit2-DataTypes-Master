1367cba888a7f60c1b0e7fd644253328c5118812
Jason-8577 <jasono119@nycstudents.net>
2025-09-26 06:22:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -35,4 +35,22 @@ def tip(y):
         print(f"{x} is odd")
 x=2 """
 
+service = input("Was the service good?")
+def bill(x,y):
+    if service == "bad":
+        print("total bill is(x+y*1)")
+    elif service == "okay":
+        print("total bill is(x+y*1.15)")
+    elif service == "good":
+        print("total bill is(x+y*1.2)")
+    else:
+        print("total bill is(x+y*1.25)")
+x=5
 
+
+
+
+
+"""
+if x % i == 0
+elif y % i == 0  """
