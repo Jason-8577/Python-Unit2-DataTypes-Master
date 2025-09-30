3a3555f11c1f5df8db32449478857ae60b1028ee
Jason-8577 <jasono119@nycstudents.net>
2025-09-30 06:23:12 -0700

    Update test.py

--- test.py
+++ test.py
@@ -9,37 +9,37 @@ not_movies(homework) """
 
 """ a = int('5')
 bill = input("How much was the bill?")
-print(int(bill))  
+print(int(bill))  """ 
 
-def factor(x,y):
+""" def factor(x,y):
     if x % y == 0:
         print(f"{y} is a factor of {x}")
 factor(20,5)
-
-def number(x):
-    if x % 2 == 0:
-        print(f"{x} is even")
-    else:
-        print(f"{x} is odd")
-x=3 
-
-service = input("Was the service good?")
-if service == "bad":
-    print("no tip," "total is 10")
-elif service == "okay":
-    print("15% tip," "total is 11.5")
-elif service == "good":
-    print("20% tip," "total is 12")
-else:
-    print("25% tip," "total is 12.5")
-x = 10 """
+ """
 
 """ factor = input("what is the number?")
 if factor == "10":
     print("1,2,5,10") """
 
+""" def number(x):
+    if x % 2 == 0:
+        print(f"{x} is even")
+    else:
+        print(f"{x} is odd")
+x=3  """
+
+""" service = input("Was the service good?")
+bills = 10
+if service == "bad":
+    x = bills*1
+elif service == "okay":
+    x = bills*1.15
+elif service == "good":
+    x = bills * 1.2
+else:
+    x = bills * 1.25
+Total = x
+print(Total) """
+
 def gcf(x,y):
-    if gcf == (10,5):
-        x % 10 == 0
-        y % 5 == 0
-z = "greatest common factor"
+    
